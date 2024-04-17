8940485dbbc837214f8615433aaf9d87d2b3af67
Oluwayomi Ilesanmi <yomilinus123@gmail.com>
2024-04-17 11:09:50 +0100

    setup folder structure and error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,47 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import knex from 'knex';
 import environmentConfig from '../knexfile';
 
-//use environmental variables
+// Use environmental variables
 import dotenv from 'dotenv';
 dotenv.config();
 
-// Extracting development environment configuration 
+//import middlewares
+import { errorHandler } from './middleware/errorMiddleware';
+
+// Extracting Connection Variables environment configuration 
 const { connectionVariables } = environmentConfig;
 
-
-// Creating a Knex instance with the development environment configuration
+// Creating a Knex instance with the Connection Variables configuration
 const db = knex(connectionVariables);
 
 // Test database connection
 db.raw('SELECT 1+1 as result')
     .then((result: any) => {
         console.log('Database connection successful');
+
+        // Creating an express app
+        const app = express();
+
+
+        // All Middlewares
+
+        // Middleware to parse json data.
+        app.use(bodyParser.json());
+
+        //Error handler middleware
+        app.use(errorHandler);
+
+
+        // Start the Express server
+        const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
     })
     .catch((error: any) => {
         console.error('Error connecting to the database:', error);
+        // Exit the process if there is an error connecting to the database
+        process.exit(1);
     });
-
-
-// creating an express app
-const app = express();
-
-// Middleware to parser json data.
-app.use(bodyParser.json());
-
-console.log(process.env.PORT)
-//start the Express server
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
---
+++ src/middleware/errorMiddleware.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    //log the error to console or third party logger for debugging purpose
+    console.error(err.stack);
+
+    // Check if headersSent is true. If true, the response headers have already been sent to the client,
+    // and it's too late to send an error response.
+    if (res.headersSent) {
+        return next(err);
+    }
+
+    // Set the status code based on the error type or default to 500 (Internal Server Error)
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    // Set the response status code and send the error message
+    res.status(statusCode).json({
+        error: {
+            message: err.message,
+            statusCode: statusCode
+        }
+    });
+};
