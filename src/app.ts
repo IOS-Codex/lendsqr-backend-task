59a955957280567b13659a3b7ef74dd73cb2668c
Oluwayomi Ilesanmi <yomilinus123@gmail.com>
2024-04-22 09:44:36 +0100

    added database test before starting server

--- src/app.ts
+++ src/app.ts
@@ -17,28 +17,31 @@ import userRoute from './routes/userRoutes';
 import walletRoute from './routes/walletRoutes';
 import transactionRoute from './routes/transactionRoutes';
 
-
 // Creating a Knex instance with the Connection Variables configuration
 const db = knex(environmentConfig);
+
 export default db;
 
-
-
+// Function to test database connection
+async function testDatabaseConnection() {
+    try {
+        await db.raw('SELECT 1');
+        console.log('Database connection successful');
+        return true;
+    } catch (error) {
+        console.error('Error connecting to database:', error);
+        return false;
+    }
+}
 
 // Creating an express app
 const app = express();
 
-
-
-
 // All Middlewares
 // use middlewares
 app.use((req: Request, res: Response, next: NextFunction) => {
-
     next()
 })
-
-
 
 // Parse JSON bodies
 app.use(express.json()); // or app.use(bodyParser.json());
@@ -49,27 +52,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Parse cookies
 app.use(cookieParser());
 
-
-
 //routes middleware
 app.use("/api/user", userRoute)
 app.use("/api/wallet", walletRoute)
 app.use("/api/transaction", transactionRoute)
 
-
-
 // Home route
 app.get("/", (req, res) => {
-    res.send("Hey, i am up and running")
+    res.send("Hey, I am up and running")
 })
-
 
 //Error handler middleware
 app.use(errorHandler);
 
+// Start the Express server after testing database connection
+async function startServer() {
+    const isConnected = await testDatabaseConnection();
+    if (isConnected) {
+        const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } else {
+        console.log('Failed to connect to the database. Server not started.');
+    }
+}
 
-// Start the Express server
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Start the server
+startServer();
